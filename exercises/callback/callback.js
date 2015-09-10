2ebead4d7caefde9bccca219edfd2f4c1b09424d
Claus Bové <clbo@kea.dk>
2015-09-10 10:23:23 +0200

    .

---
+++ exercises/callback/callback.js
@@ -0,0 +1,11 @@
+// through 4 console log statements you should print out
+
+// I
+// Am
+// On time
+// And invisible! 
+
+// you should make use of 2 setTimeout() functions in your solution
+
+// try it out in the Loupe machine:
+// http://latentflip.com/loupe/
